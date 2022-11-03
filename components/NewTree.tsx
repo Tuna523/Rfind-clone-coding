8f75f85822c1b6849550ecc5dba9e6b0c8ca3a3c
Tuna523 <duya1010@naver.com>
2022-11-03 16:26:12 +0900

    Tree 열고 닫기 버튼 추가

--- components/NewTree.tsx
+++ components/NewTree.tsx
@@ -1,27 +1,33 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import NewTreeItem from "./NewTreeItem";
 
 
 const NewTree:React.FC<{}> = () => {
+    const [showStatus, setShowStatus] = useState(false);
     const { data:props } = useQuery(['legal-dong-item-root'], () => axios.get(`https://rfind-api-int.rsquare.co.kr/legal-dongs`));
 
     return(
+        <>
+        {
         <div className="treeContainerPadding">
-            <div style={{background: 'rgba(207, 237, 246, 0.4)'}} className="treeContainer">
-            {
-                props?.data && props.data.length > 0 &&
-                    <ul>
-                        {props.data.map((data: Object, idx: number)=> {
-                            return <li key={`root-item-${idx}`}>
-                                <NewTreeItem value={data} {...props}></NewTreeItem>
-                            </li>
-                        })}
-                    </ul>
-            }
+            <span onClick={(e)=> {setShowStatus(!showStatus)}}>{showStatus ? 'Click to Close' : 'Click to Open'}</span>
+                <div style={{display: showStatus? '' : 'none', background: 'rgba(207, 237, 246, 0.4)'}} className="treeContainer">
+                {
+                    props?.data && props.data.length > 0 &&
+                        <ul>
+                            {props.data.map((data: Object, idx: number)=> {
+                                return <li key={`root-item-${idx}`}>
+                                    <NewTreeItem value={data} {...props}></NewTreeItem>
+                                </li>
+                            })}
+                        </ul>
+                }
+                </div>
             </div>
-        </div>
+        }
+        </>
     )
 }
 
